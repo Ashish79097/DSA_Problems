fd15d6f2a7fd0198c848c7159b15301315731f31
Ashish79097 <ashishsharma79097@gmail.com>
2026-08-16 23:26:33 +0530

    Time: 6 ms (54.51%), Space: 47.3 MB (52.87%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
@@ -1,26 +1,26 @@
 class Solution {
-    public int[] findIntersectionValues(int[] nums1, int[] nums2) {
-        int[] arr = new int[2];
+    public int[] findIntersectionValues(int[] arr1, int[] arr2) {
         int count1=0;
         int count2=0;
-        for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]){
-                    count1++;
-                    break;
+        for(int i=0;i<arr1.length;i++){
+            for(int j=0;j<arr2.length;j++){
+                if(arr1[i] ==arr2[j]){ 
+                count1++;
+                break;
                 }
             }
         }
-         for(int j=0;j<nums2.length;j++){
-            for(int i=0;i<nums1.length;i++){
-                if(nums1[i]==nums2[j]){
-                    count2++;
-                    break;
+         for(int i=0;i<arr2.length;i++){
+            for(int j=0;j<arr1.length;j++){
+                if(arr2[i] ==arr1[j]) {
+                count2++;
+                break;
                 }
             }
         }
-        arr[0] = count1;
-        arr[1] = count2;
-        return arr;
-    }    
+        int[] res = new int[2];
+        res[0] = count1;
+        res[1] = count2;
+        return res;
+    }
 }
